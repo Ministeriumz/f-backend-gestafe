4056753e6dd6d0f1350bb93b7cae540ad9eeed3f
joao-pedro-chapichi <chapichijp@gmail.com>
2025-09-16 14:14:42 -0300

    feat: Adicionando ENDPOINT PATCH

---
+++ src/Objects/Dtos/Entities/IgrejaPatchDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace f_backend_gestafe.src.Objects.Dtos.Entities
+{
+    public class IgrejaPatchDTO
+    {
+        public string? Nome { get; set; }
+        public string? Cnpj { get; set; }
+        public string? Estado { get; set; }
+        public string? Rua { get; set; }
+        public string? Cep { get; set; }
+        public string? Numero { get; set; }
+    }
+}
