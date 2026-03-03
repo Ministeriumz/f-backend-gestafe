ab80996819afd27d0188a090a7084a8779d71907
joao-pedro-chapichi <chapichijp@gmail.com>
2026-03-02 21:41:11 -0300

    Adicionando CRUD FINANCEIRO e ENUM STATUS

---
+++ Data/Builders/FinanceiroBuilder.cs
@@ -0,0 +1,17 @@
+﻿using f_backend_gestafe.Objects.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace f_backend_gestafe.Data.Builders
+{
+    public class FinanceiroBuilder
+    {
+        public static void Build(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Financeiro>().HasKey(i => i.Id);
+            modelBuilder.Entity<Financeiro>().Property(i => i.Valor).IsRequired();
+            modelBuilder.Entity<Financeiro>().Property(i => i.Acao).IsRequired();
+            modelBuilder.Entity<Financeiro>().Property(i => i.Data).IsRequired();
+            modelBuilder.Entity<Financeiro>().Property(i => i.Status).IsRequired();
+        }
+    }
+}
---
+++ Objects/Dtos/Entities/FinanceiroDTO.cs
@@ -0,0 +1,14 @@
+﻿using f_backend_gestafe.Objects.Enums;
+
+namespace f_backend_gestafe.Objects.Dtos.Entities
+{
+    public class FinanceiroDTO
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public string Acao { get; set; }
+        public DateTime Data { get; set; }
+        public StatusFinanceiro Status { get; set; }
+        public int IgrejaId { get; set; }
+    }
+}
